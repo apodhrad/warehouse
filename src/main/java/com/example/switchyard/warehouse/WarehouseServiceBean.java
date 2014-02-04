f6e7f879afc94a98fb0b72a7fb51e6c04048a54f
Andrej Podhradsky <apodhrad@redhat.com>
2014-02-04 11:14:57 +0100

    Extended API info

--- src/main/java/com/example/switchyard/warehouse/WarehouseServiceBean.java
+++ src/main/java/com/example/switchyard/warehouse/WarehouseServiceBean.java
@@ -42,6 +42,22 @@ public class WarehouseServiceBean implements WarehouseService {
 				+ "<a href=\"http://localhost:8080/warehouse/items\">http://localhost:8080/warehouse/items</a>"
 				+ "<h3>Get an item:</h3>"
 				+ "<a href=\"http://localhost:8080/warehouse/items/ITEM001\">http://localhost:8080/warehouse/items/ITEM001</a>"
+				+ "<h3>ItemService</h3><pre>@Path(\"/warehouse\")\n"
+				+ "public interface ItemService {\n\n"
+				+ "  @GET\n  @Path(\"/items/{itemId}\")\n"
+				+ "  Item getItem(@PathParam(\"itemId\") String itemId);\n"
+				+ "  }\n}</pre>"
+				+ "<h3>Item</h3><pre>@XmlRootElement\n@JsonIgnoreProperties(ignoreUnknown = true)\n"
+				+ "public class Item {\n\n"
+				+ "  private String itemId;\n  private String description;\n  private int quantity;\n\n"
+				+ "  public String getItemId() {\n    return itemId;\n  }\n\n"
+				+ "  public void setItemId(String itemId) {\n    this.itemId = itemId;\n  }\n\n"
+				+ "  public String getDescription() {\n    return description;\n  }\n\n"
+				+ "  public void setDescription(String description) {\n    this.description = description;\n  }\n\n"
+				+ "  public int getQuantity() {\n    return quantity;\n  }\n\n"
+				+ "  public void setQuantity(int quantity) {\n    this.quantity = quantity;\n  }\n"
+				+ "}</pre>"
+
 				+ "</html>";
 	}
 
